220356ddd0717f968093d03e3a5192d45c52dbda
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2020-07-07 18:13:46 +0300

    More customizations

--- README.md
+++ README.md
@@ -8,8 +8,9 @@ Tinc VPN 1.1 @ Alpine
 
 ```bash
 docker run -it -d --name=tinc \
+    -e NETWORK_NAME=vpn \
     -e NETWORK_ADDRESS=10.250.0.1 \
-    -e NETWORK_MASK=24 \
+    -e NETWORK_PREFIX=24 \
     -e NODE_NAME=myhostname \
     -e SERVER=1 \
     --net=host \
@@ -21,8 +22,9 @@ docker run -it -d --name=tinc \
 
 This will create a network called `vpn` (10.250.0.0/24), and set 10.250.0.1 as node's IP address.
 
+  * `BETWORK_NAME` (default: `vpn`): [network name](https://manpages.debian.org/experimental/tinc/tinc.conf.5.en.html#NAMES)
   * `NETWORK_ADDRESS`: IP address for the node (must be unique for every node)
-  * `NETWORK_MASK` (24 if omitted): network address prefix
+  * `NETWORK_PREFIX` (24 if omitted): network address prefix
   * `NODE_NAME`: node identifier for tinc ([only alphanumeric characters and underscore are allowed](https://manpages.debian.org/experimental/tinc/tinc.conf.5.en.html#NAMES))
   * `SERVER`: must be set to a non-empty string for the primary server
 
@@ -47,7 +49,7 @@ You will need the last line of the output, this is the secret link to join the V
 ```bash
 docker run -it -d --name=tinc \
     -e NETWORK_ADDRESS=10.250.0.2 \
-    -e NETWORK_MASK=24 \
+    -e NETWORK_PREFIX=24 \
     -e INVITE_URL=that_long_invitation_url_from_the_previous_step \
     --net=host \
     --device=/dev/net/tun \
@@ -57,6 +59,6 @@ docker run -it -d --name=tinc \
     wildwildangel/tinc:latest
 ```
 
-`NETWORK_MASK` variables should match across all nodes, `NETWORK_ADDRESS` must obviously be unique.
+`NETWORK_PREFIX` variables should match across all nodes, `NETWORK_ADDRESS` must obviously be unique.
 
 Please note that `SERVER` environment variable must not be passed to client nodes (or at least be empty). `NODE_NAME` is not used, because the client will receive its name from the server.
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-if [ ! -d /etc/tinc/vpn ]; then
+NETWORK=${NETWORK_NAME:-vpn}
+
+if [ ! -d "/etc/tinc/${NETWORK}" ]; then
     if [ -z "${NETWORK_ADDRESS}" ]; then
         echo Please set NETWORK_ADDRESS environment variable
         exit 1
     fi
 
     if [ -n "${SERVER}" ]; then
-        /usr/sbin/tinc -n vpn init "${NODE_NAME:-$(hostname)}"
-        cat > /etc/tinc/vpn/invitation-created <<'EOF'
+        /usr/sbin/tinc -n "${NETWORK}" init "${NODE_NAME:-$(hostname)}"
+        cat > "/etc/tinc/${NETWORK}/invitation-created" <<'EOF'
 #!/bin/sh
 
 cat > $INVITATION_FILE <<EOT
@@ -19,28 +21,28 @@ ConnectTo = $NAME
 EOT
 /usr/sbin/tinc export-all >> $INVITATION_FILE
 EOF
-        chmod +x /etc/tinc/vpn/invitation-created
+        chmod +x "/etc/tinc/${NETWORK}/invitation-created"
     elif [ -n "${INVITE_URL}" ]; then
         /usr/sbin/tinc join "${INVITE_URL}"
     fi
 
-    /usr/sbin/tinc -n vpn add subnet ${NETWORK_ADDRESS}
-    if [ -d /etc/tinc/vpn ]; then
-        cat > /etc/tinc/vpn/tinc-up <<EOF
+    /usr/sbin/tinc -n "${NETWORK}" add subnet ${NETWORK_ADDRESS}
+    if [ -d "/etc/tinc/${NETWORK}" ]; then
+        cat > "/etc/tinc/${NETWORK}/tinc-up" <<EOF
 #!/bin/sh
 
-/sbin/ip addr add ${NETWORK_ADDRESS}/${NETWORK_MASK:-24} dev \$INTERFACE
+/sbin/ip addr add ${NETWORK_ADDRESS}/${NETWORK_PREFIX:-24} dev \$INTERFACE
 /sbin/ip link set \$INTERFACE up
 EOF
 
-        cat > /etc/tinc/vpn/tinc-down <<EOF
+        cat > "/etc/tinc/${NETWORK}/tinc-down" <<EOF
 #!/bin/sh
 
 /sbin/ip link set \$INTERFACE down
-/sbin/ip addr del ${NETWORK_ADDRESS}/${NETWORK_MASK:-24} dev \$INTERFACE
+/sbin/ip addr del ${NETWORK_ADDRESS}/${NETWORK_PREFIX:-24} dev \$INTERFACE
 EOF
 
-        chmod +x /etc/tinc/vpn/tinc-up /etc/tinc/vpn/tinc-down
+        chmod +x "/etc/tinc/${NETWORK}/tinc-up" "/etc/tinc/${NETWORK}/tinc-down"
     fi
 fi
 
