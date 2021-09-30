2a869c0ccdc21ae324cae1f338b916b96553bdf8
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2021-10-01 02:44:35 +0300

    Refactoring

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,16 @@
-FROM alpine:3.12
-RUN apk add --no-cache tinc-pre
+FROM alpine:3.14
+ARG vpn_network=vpn
+
+RUN \
+    apk --no-cache upgrade && \
+    apk add --no-cache tinc-pre=~1.1
+
+COPY ./entrypoint.sh /entrypoint.sh
+
+ENV NETWORK_NAME=$vpn_network
+
 EXPOSE 655/tcp 655/udp
 VOLUME /etc/tinc
-COPY ./entrypoint.sh /entrypoint.sh
+
 ENTRYPOINT ["/entrypoint.sh"]
-CMD ["start", "-D", "--logfile", "/dev/fd/2", "-n", "vpn"]
+CMD ["start", "-D", "--logfile", "/dev/fd/2"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,49 +1,46 @@
 #!/bin/sh
 
-NETWORK=${NETWORK_NAME:-vpn}
+: "${NETWORK_NAME:?"NETWORK_NAME environment variable is not set"}"
 
-if [ ! -d "/etc/tinc/${NETWORK}" ]; then
-    if [ -z "${NETWORK_ADDRESS}" ]; then
-        echo Please set NETWORK_ADDRESS environment variable
-        exit 1
-    fi
+if [ ! -d "/etc/tinc/${NETWORK_NAME}" ]; then
+    : "${NETWORK_ADDRESS:?"NETWORK_ADDRESS environment variable is not set"}"
 
-    if [ -n "${SERVER}" ]; then
-        /usr/sbin/tinc -n "${NETWORK}" init "${NODE_NAME:-$(hostname)}"
-        cat > "/etc/tinc/${NETWORK}/invitation-created" <<'EOF'
+    if [ -n "${SERVER:-}" ]; then
+        /usr/sbin/tinc -n "${NETWORK_NAME}" init "${NODE_NAME:-$(hostname)}"
+        cat > "/etc/tinc/${NETWORK_NAME}/invitation-created" <<'EOF'
 #!/bin/sh
 
-cat > $INVITATION_FILE <<EOT
-Name = $NODE
-Netname = $NETNAME
-ConnectTo = $NAME
+cat > "${INVITATION_FILE}" <<EOT
+Name = ${NODE}
+Netname = ${NETNAME}
+ConnectTo = ${NAME}
 #----------------#
 EOT
-/usr/sbin/tinc export-all >> $INVITATION_FILE
+/usr/sbin/tinc export-all >> "${INVITATION_FILE}"
 EOF
-        chmod +x "/etc/tinc/${NETWORK}/invitation-created"
-    elif [ -n "${INVITE_URL}" ]; then
+        chmod +x "/etc/tinc/${NETWORK_NAME}/invitation-created"
+    elif [ -n "${INVITE_URL:-}" ]; then
         /usr/sbin/tinc join "${INVITE_URL}"
     fi
 
-    /usr/sbin/tinc -n "${NETWORK}" add subnet ${NETWORK_ADDRESS}
-    if [ -d "/etc/tinc/${NETWORK}" ]; then
-        cat > "/etc/tinc/${NETWORK}/tinc-up" <<EOF
+    /usr/sbin/tinc -n "${NETWORK_NAME}" add subnet "${NETWORK_ADDRESS}"
+    if [ -d "/etc/tinc/${NETWORK_NAME}" ]; then
+        cat > "/etc/tinc/${NETWORK_NAME}/tinc-up" <<EOF
 #!/bin/sh
 
 /sbin/ip addr add ${NETWORK_ADDRESS}/${NETWORK_PREFIX:-24} dev \$INTERFACE
 /sbin/ip link set \$INTERFACE up
 EOF
 
-        cat > "/etc/tinc/${NETWORK}/tinc-down" <<EOF
+        cat > "/etc/tinc/${NETWORK_NAME}/tinc-down" <<EOF
 #!/bin/sh
 
 /sbin/ip link set \$INTERFACE down
 /sbin/ip addr del ${NETWORK_ADDRESS}/${NETWORK_PREFIX:-24} dev \$INTERFACE
 EOF
 
-        chmod +x "/etc/tinc/${NETWORK}/tinc-up" "/etc/tinc/${NETWORK}/tinc-down"
+        chmod +x "/etc/tinc/${NETWORK_NAME}/tinc-up" "/etc/tinc/${NETWORK_NAME}/tinc-down"
     fi
 fi
 
-exec /usr/sbin/tinc $@
+exec /usr/sbin/tinc -n "${NETWORK_NAME}" "$@"
